03498435d34c7be3569496fd05c21291a31a1ae1
NDA_ADN <nguyendinhan97@gmail.com>
2018-04-23 20:47:06 +0700

    Finish user edit, update, index and destroy actions

--- Gemfile
+++ Gemfile
@@ -33,6 +33,9 @@ gem 'jbuilder', '~> 2.5'
 gem 'bootstrap-sass', '3.3.7'
 gem 'bcrypt',         '3.1.11'
 gem 'jquery-rails', '4.3.1'
+gem 'faker',          '>= 1.7.3'
+gem 'will_paginate',           '3.1.6'
+gem 'bootstrap-will_paginate', '1.0.0'
 
 group :development, :test do
   gem 'sqlite3', '1.3.13'
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,8 @@ class User < ApplicationRecord
 	validates :name, presence: true, length: { maximum: 50 }
 	validates :email, presence: true, length: { maximum: 255 }, format: {with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
 	has_secure_password
-	validates :password, presence: true, length: { minimum: 6 }
 
+	validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
 	# Returns the hash digest of the given string.
 	def User.digest(string)
