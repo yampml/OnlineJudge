a6a9fc0058e66fc70b59f39a56fa6a3955bdf254
NDA_ADN <nguyendinhan97@gmail.com>
2018-04-26 23:47:12 +0700

    commit commit

--- config/routes.rb
+++ config/routes.rb
@@ -32,7 +32,9 @@ Rails.application.routes.draw do
   scope '/admin' do
     resources :problems, only: [:new, :edit, :create, :update, :destroy]
   end
-  
+
+  resources :submissions, 
+
   
 
 end
