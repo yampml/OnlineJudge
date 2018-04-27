ea1693336658ced60fe4dd6eb208253785388c57
LeDSinh <dinhsinh.le@outlook.com>
2018-04-27 23:46:52 +0700

    Moi add khung cho table problem set

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: problems
+#
+#  id         :bigint(8)        not null, primary key
+#  name       :string
+#  timelimit  :float
+#  memlimit   :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  file_name  :json
+#
+
 class Problem < ApplicationRecord
 	has_many :submissions
 
