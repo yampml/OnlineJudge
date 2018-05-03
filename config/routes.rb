2558d21ae4d36e63f3b15b5d8f79e417c8c08cb5
LeDSinh <dinhsinh.le@outlook.com>
2018-05-03 16:23:08 +0700

    trash file

--- config/routes.rb
+++ config/routes.rb
@@ -1,55 +1,54 @@
 # == Route Map
 #
-#           Prefix Verb   URI Pattern                        Controller#Action
-#       problemset GET    /problems/index(.:format)          problems#index
-#    problems_show GET    /problems/show(.:format)           problems#show
-#     problems_new GET    /problems/new(.:format)            problems#new
-#    problems_edit GET    /problems/edit(.:format)           problems#edit
-#  problems_create GET    /problems/create(.:format)         problems#create
-#  problems_update GET    /problems/update(.:format)         problems#update
-# problems_destroy GET    /problems/destroy(.:format)        problems#destroy
-#     sessions_new GET    /sessions/new(.:format)            sessions#new
-#             root GET    /                                  static_pages#home
-#             help GET    /help(.:format)                    static_pages#help
-#            about GET    /about(.:format)                   static_pages#about
-#          contact GET    /contact(.:format)                 static_pages#contact
-#           signup GET    /signup(.:format)                  users#new
-#                  POST   /signup(.:format)                  users#create
-#            login GET    /login(.:format)                   sessions#new
-#                  POST   /login(.:format)                   sessions#create
-#           logout DELETE /logout(.:format)                  sessions#destroy
-#            users GET    /users(.:format)                   users#index
-#                  POST   /users(.:format)                   users#create
-#         new_user GET    /users/new(.:format)               users#new
-#        edit_user GET    /users/:id/edit(.:format)          users#edit
-#             user GET    /users/:id(.:format)               users#show
-#                  PATCH  /users/:id(.:format)               users#update
-#                  PUT    /users/:id(.:format)               users#update
-#                  DELETE /users/:id(.:format)               users#destroy
-#            blogs POST   /blogs(.:format)                   blogs#create
-#             blog DELETE /blogs/:id(.:format)               blogs#destroy
-#         problems POST   /admin/problems(.:format)          problems#create
-#      new_problem GET    /admin/problems/new(.:format)      problems#new
-#     edit_problem GET    /admin/problems/:id/edit(.:format) problems#edit
-#          problem PATCH  /admin/problems/:id(.:format)      problems#update
-#                  PUT    /admin/problems/:id(.:format)      problems#update
-#                  DELETE /admin/problems/:id(.:format)      problems#destroy
-#      submissions POST   /submissions(.:format)             submissions#create
-#   new_submission GET    /submissions/new(.:format)         submissions#new
+#          Prefix Verb   URI Pattern                             Controller#Action
+#      problemset GET    /problems/index(.:format)               problems#index
+#         problem GET    /problems/:id(.:format)                 problems#show
+#    problems_new GET    /problems/new(.:format)                 problems#new
+#   problems_edit GET    /problems/edit(.:format)                problems#edit
+# problems_create GET    /problems/create(.:format)              problems#create
+# problems_update GET    /problems/update(.:format)              problems#update
+#                 DELETE /problems/:id(.:format)                 problems#destroy
+#    sessions_new GET    /sessions/new(.:format)                 sessions#new
+#            root GET    /                                       static_pages#home
+#            help GET    /help(.:format)                         static_pages#help
+#           about GET    /about(.:format)                        static_pages#about
+#         contact GET    /contact(.:format)                      static_pages#contact
+#          signup GET    /signup(.:format)                       users#new
+#                 POST   /signup(.:format)                       users#create
+#           login GET    /login(.:format)                        sessions#new
+#                 POST   /login(.:format)                        sessions#create
+#          logout DELETE /logout(.:format)                       sessions#destroy
+#           users GET    /users(.:format)                        users#index
+#                 POST   /users(.:format)                        users#create
+#        new_user GET    /users/new(.:format)                    users#new
+#       edit_user GET    /users/:id/edit(.:format)               users#edit
+#            user GET    /users/:id(.:format)                    users#show
+#                 PATCH  /users/:id(.:format)                    users#update
+#                 PUT    /users/:id(.:format)                    users#update
+#                 DELETE /users/:id(.:format)                    users#destroy
+#           blogs POST   /blogs(.:format)                        blogs#create
+#            blog DELETE /blogs/:id(.:format)                    blogs#destroy
+#     submissions POST   /problems/:id/submissions(.:format)     submissions#create
+#  new_submission GET    /problems/:id/submissions/new(.:format) submissions#new
+#        problems GET    /problems(.:format)                     problems#index
+#                 POST   /problems(.:format)                     problems#create
+#     new_problem GET    /problems/new(.:format)                 problems#new
+#    edit_problem GET    /problems/:id/edit(.:format)            problems#edit
+#                 GET    /problems/:id(.:format)                 problems#show
+#                 PATCH  /problems/:id(.:format)                 problems#update
+#                 PUT    /problems/:id(.:format)                 problems#update
+#                 DELETE /problems/:id(.:format)                 problems#destroy
+#                 POST   /admin/problems(.:format)               problems#create
+#                 GET    /admin/problems/new(.:format)           problems#new
+#                 GET    /admin/problems/:id/edit(.:format)      problems#edit
+#                 PATCH  /admin/problems/:id(.:format)           problems#update
+#                 PUT    /admin/problems/:id(.:format)           problems#update
+#                 DELETE /admin/problems/:id(.:format)           problems#destroy
 
 Rails.application.routes.draw do
   get 'problems/index', to: 'problems#index', as: 'problemset'
 
   get 'problems/:id', to: 'problems#show', as: 'problem' 
-
-  get 'problems/new'
-
-  get 'problems/edit'
-
-  get 'problems/create'
-
-  get 'problems/update'
-
   
   delete 'problems/:id', to: 'problems#destroy'
 
@@ -78,7 +77,7 @@ Rails.application.routes.draw do
 
   scope '/admin' do
     resources :problems, only: [:new, :edit, :create, :update, :destroy]
-  end
+  end 
 
   # resources :submissions, only: [:new, :create]
 
