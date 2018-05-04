3e0879a90df4ff26eed891c74e1ad1005c139ff1
LeDSinh <dinhsinh.le@outlook.com>
2018-05-04 15:20:34 +0700

    sua mot chut o quyen delete blog

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -22,7 +22,7 @@ class BlogsController < ApplicationController
 	private
 
 		def blog_params
-			params.require(:blog).permit(:content)
+			params.require(:blog).permit(:title, :content)
 		end
 
 		def correct_user
